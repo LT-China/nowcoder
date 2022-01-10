04aed732c47a126fe96ddd22ece32ffd768abf95
tyb <tybtang@outlook.com>
2022-01-10 22:33:24 +0800

    update

--- src/hw/middle/HJ38.py
+++ src/hw/middle/HJ38.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def heightFunc(tmpheight):
+    print(round(23/8, 6) * tmpheight)
+    print(round(1/32, 6) * tmpheight)
+
+heightFunc(int(input()))
