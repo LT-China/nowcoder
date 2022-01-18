bf47d3747e71098efbc82d7ed14090faa449b749
tyb <LeoTang.China@outlook.com>
2022-01-18 17:57:31 +0800

    测试用例没有全部通过, 需重新优化逻辑

--- src/hw/easy/HJ22-todo.py
+++ src/hw/easy/HJ22-todo.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#TODO
+def bottlecal(emptybottlenum):
+    # 空瓶数
+    # emptybottlenum = 0
+    # 汽水瓶
+    #sodabottlenumsum = 0
+    # 置换的空瓶
+    switchemptybottle = emptybottlenum
+    if switchemptybottle >= 2:
+        # 第一次置换
+        sodabottlenum = emptybottlenum // 3
+        switchemptybottle = sodabottlenum + emptybottlenum % 3
+        sodabottlenumsum = sodabottlenum
+        # 第一次置换后
+        while switchemptybottle > 2:
+            sodabottlenum = switchemptybottle // 3
+            switchemptybottle = sodabottlenum + switchemptybottle % 3 + 1
+            sodabottlenumsum = sodabottlenumsum + sodabottlenum
+        print(sodabottlenumsum)
+    else:
+        print(0)
+
+bottlecal(int(input()))
+# while True:
+#     try:
+#         bottlecal(int(input()))
+#     except:
+#         break
